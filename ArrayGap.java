6cde67a10a35d4a2a6d6754845fd1bad0acde6d5
DHplusplusCoder <104809063+DHplusplusCoder@users.noreply.github.com>
2022-09-30 08:17:01 -0700

    Add files via upload

---
+++ ArrayGap.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author drew
+ */
+public class ArrayGap {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Given an array of integers, return the largest gap between the sorted elements of the array.
+        
+        int[] Array = {1,2,3,9,10);
+        
+        for(int j = 0; j < 4; j++)
+        {
+            int Difference = abs(Array[j] - Array[j+1]);
+            if(Difference > 1)
+            System.out.println(Difference);
+        }
+    }
+    }
+    
+}
